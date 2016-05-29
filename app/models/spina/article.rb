fb23c08d468dbdd995620fce41efcbdae8177adc
Renaldo Pierre-Louis <pirelouisd87@gmail.com>
2016-05-29 08:37:40 -0400

    removing to_param, because it breaks admin side

--- app/models/spina/article.rb
+++ app/models/spina/article.rb
@@ -4,10 +4,6 @@ module Spina
     validates :title, :subtitle, :content, :publish_date, presence: true
     belongs_to :category
 
-    def to_param
-      slug
-    end
-
     def slug
       super || self.title.parameterize
     end
