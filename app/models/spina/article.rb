a0fcb1a5fc665986a7b5c66325747f6dbae76cd1
Renaldo Pierre-Louis <pirelouisd87@gmail.com>
2016-06-02 21:22:28 -0400

    persists slug in database

--- app/models/spina/article.rb
+++ app/models/spina/article.rb
@@ -1,12 +1,15 @@
 module Spina
   class Article < ActiveRecord::Base
+    before_save :create_slug
     mount_uploader :header_photo, PhotoUploader
     validates :title, :subtitle, :content, :publish_date, presence: true
     belongs_to :category
     belongs_to :author
 
-    def slug
-      super || self.title.parameterize
+    private
+
+    def create_slug
+      self.slug = self.title.parameterize unless self.slug.present?
     end
   end
 end
